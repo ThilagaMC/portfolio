8ad5a1204fd660764fe8024c817408d373260dd5
ThilagaMC <thilaga2k2@gmail.com>
2025-07-03 14:09:08 +0530

    vite update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,11 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.svg']
-})
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.svg'],
+  build: {
+    assetsInlineLimit: 4096
+  }
+});
