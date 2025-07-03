374d35098c2eb5a5591ea8191ca38048735da521
ThilagaMC <thilaga2k2@gmail.com>
2025-07-03 16:12:04 +0530

    config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.PNG', '**/*.png', '**/*.SVG', '**/*.svg'],
+  assetsInclude: [
+    '**/*.JPG',
+    '**/*.JPEG',
+    '**/*.PNG',
+    '**/*.SVG',
+    '**/*.jpg',
+    '**/*.jpeg',
+    '**/*.png',
+    '**/*.svg'
+  ]
 })
